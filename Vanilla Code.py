7b6e3f0ad8d669c84ae16fbf4b06f6430263d8e1
SydTri <s2753367@ed.ac.uk>
2024-10-28 10:57:30 +0000

    create small database and time schedule

--- Vanilla Code.py
+++ Vanilla Code.py
@@ -14,6 +14,17 @@ ch_2_conversion_rates = pd.read_csv('/workspaces/mmcsfinalproject/channel_2_conv
 ch_2_schedule = pd.read_csv('/workspaces/mmcsfinalproject/channel_2_schedule.csv')
 ch_A_schedule = pd.read_csv('/workspaces/mmcsfinalproject/channel_A_schedule.csv')
 movie_database = pd.read_csv('/workspaces/mmcsfinalproject/movie_database.csv')
+movie_database_small = movie_database.head(50)
 
 #Testing that it worked
-print(ch_0_conversion_rates.head(5))
+#print(ch_0_conversion_rates.head(5))
+print(movie_database_small)
+
+#create time_schedule
+time_schedule = (ch_A_schedule.iloc[:, :1])
+#print(time_schedule)
+
+#Pull movie, take run time and divide into time intervals
+
+
+
